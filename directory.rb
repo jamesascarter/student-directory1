0e47d8fac9b13e48ee5b1bc91ce94a5841200f0a
jamesascarter <jamesascarter@gmail.com>
2014-09-10 10:56:39 +0100

    Enter user data

--- directory.rb
+++ directory.rb
@@ -31,6 +31,22 @@ def print_footer(names)
   puts "Overall, we have #{names.length} great students."
 end
 
+def input_students
+  puts "Please enter the names of the students"
+  puts "To finish, just hit return twice"
+
+  students = []
+
+  name = gets.chomp
+  while !name.empty? do
+  	students << {:name => name, :cohort => :november}
+  	puts "Now we have #{students.length} students"
+  	name = gets.chomp
+ end
+
+   students
+ end
+students = input_students
 print_header
 print(students)
 print_footer(students)
